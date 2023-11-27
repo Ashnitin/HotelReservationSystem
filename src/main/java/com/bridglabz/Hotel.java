233a745249b77f095a3a402553f7a40c9e051f16
unknown <ashwinijadhav129aj@gmail.com>
2023-11-27 14:37:52 +0530

    Added:HotelwithNameandRateforRegularCustomer

---
+++ src/main/java/com/bridglabz/Hotel.java
@@ -0,0 +1,16 @@
+package com.bridglabz;
+
+public class Hotel {
+    private String hotelName;
+    private String regularCustomer;
+    private int rate;
+
+    public Hotel(String hotelName, String regularCustomer, int rate) {
+        this.hotelName = hotelName;
+        this.regularCustomer = regularCustomer;
+        this.rate = rate;
+    }
+
+    public Hotel() {
+    }
+}
--- src/main/java/com/bridglabz/HotelReservationMain.java
+++ src/main/java/com/bridglabz/HotelReservationMain.java
@@ -1,7 +1,12 @@
 package com.bridglabz;
 
 public class HotelReservationMain {
+    public void addHotel(){
+
+    }
     public static void main(String[] args) {
-        System.out.println("Welcom to Hotel Reservation");
+        Hotel h1=new Hotel("Lakewood","Regular",110);
+        Hotel h2=new Hotel("BridgeWood","Regular",160);
+        Hotel h3=new Hotel("RidgeWood","regular",220);
     }
 }
