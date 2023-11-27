c91b9d012a87f1656dde5f20e2fb65d99fe65c3a
unknown <ashwinijadhav129aj@gmail.com>
2023-11-28 00:04:11 +0530

    Added:find cheapest rated hotel for reward customer

--- src/main/java/com/bridglabz/HotelReservationMain.java
+++ src/main/java/com/bridglabz/HotelReservationMain.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class HotelReservationMain {
     LocalDate checkIn= LocalDate.of(2020, 9, 11);
     LocalDate checkOut= LocalDate.of(2020, 9, 12);
+    String dateRangeInput = "11Sep2020, 12Sep2020";
     DayOfWeek ci=checkIn.getDayOfWeek();
 
     DayOfWeek co=checkOut.getDayOfWeek();
@@ -28,10 +29,11 @@ public class HotelReservationMain {
         h.addHotel(lakeWood);
         h.addHotel(bridgeWood);
         h.addHotel(ridgeWood);
-        h.calculateRateForRegularCustomer();
-        h.bestRatedHotel();
+       // h.calculateRateForRegularCustomer();
+        //h.bestRatedHotel();
+        h.cheapestHotelRewardCustomer();
     }
-    public void calculateRateForRegularCustomer(){
+    /*public void calculateRateForRegularCustomer(){
         int sumLakewood=hotelList.get(0).calculateRateForCustomer(String.valueOf(ci))+hotelList.get(0).calculateRateForCustomer(String.valueOf(co));
         System.out.println("LakWood: "+"$"+sumLakewood);
         int sumBridgeWood=hotelList.get(1).calculateRateForCustomer(String.valueOf(ci))+hotelList.get(1).calculateRateForCustomer(String.valueOf(co));
@@ -84,6 +86,44 @@ public class HotelReservationMain {
             System.out.println("Best Rated Hotel is RidgeWood: "+ridgeWoodRating);
             System.out.println("Rate is: "+sumRidgeWood);
             System.out.println("*******************************************************************");
+        }
+    }*/
+    public void cheapestHotelRewardCustomer(){
+
+        try {
+            String[] dateRange = dateRangeInput.split(", ");
+            int sumLakewood=hotelList.get(0).calculateRateRewardCustomer(String.valueOf(ci))+hotelList.get(0).calculateRateRewardCustomer(String.valueOf(co));
+            System.out.println("LakWood: "+"$"+sumLakewood);
+            int sumBridgeWood=hotelList.get(1).calculateRateRewardCustomer(String.valueOf(ci))+hotelList.get(1).calculateRateRewardCustomer(String.valueOf(co));
+            System.out.println("BridgeWood: "+"$"+sumBridgeWood);
+            int sumRidgeWood=hotelList.get(2).calculateRateRewardCustomer(String.valueOf(ci))+hotelList.get(2).calculateRateRewardCustomer(String.valueOf(co));
+            System.out.println("RidgeWood: "+"$"+sumRidgeWood);
+            int lakeWoodRating=hotelList.get(0).calculateRating(3);
+            int bridgeWoodRating=hotelList.get(1).calculateRating(4);
+            int ridgeWoodRating=hotelList.get(2).calculateRating(5);
+            if(dateRange.length != 2)
+            {
+                throw new IllegalArgumentException("Invalid date range input. Format: <startDate>, <endDate>");
+            }
+            if(sumLakewood<=sumBridgeWood && sumBridgeWood<=sumRidgeWood &&lakeWoodRating>bridgeWoodRating){
+                System.out.println("*******************************************************************");
+                System.out.println("Cheapest best Hotel for reward customer is LakeWood: " + "$" + sumLakewood);
+                System.out.println("Rating is: "+lakeWoodRating);
+                System.out.println("*******************************************************************");
+            } else if (sumBridgeWood<=sumLakewood&&sumBridgeWood<=sumRidgeWood ) {
+                System.out.println("Cheapest Hotel for reward customer BridgeWood: "+"$"+sumBridgeWood);
+                System.out.println("Rating is: "+bridgeWoodRating);
+            }
+            else {
+                System.out.println("*******************************************************************");
+                System.out.println("Cheapest Hotel for reward customer RidgeWood: "+"$"+sumRidgeWood);
+                System.out.println("Rating is: "+ridgeWoodRating);
+                System.out.println("*******************************************************************");
+            }
+
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
